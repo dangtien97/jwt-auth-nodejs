a0142e22bd06798cd6f35119de52afda0b8291ba
Nguyễn Đăng Tiến <ilikehitech.vn@gmail.com>
2018-10-27 10:07:19 +0000

    Test add cors to router

--- auth/AuthController.js
+++ auth/AuthController.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors')
 const bodyParser = require('body-parser');
 const jwt = require('jsonwebtoken');
 const bcrypt = require('bcryptjs');
@@ -16,10 +17,9 @@ router.use(bodyParser.urlencoded(
 ));
 router.use(bodyParser.json());
 
+router.use(cors());
+
 router.use(function(req, res, next) {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   res.header('Access-Control-Allow-Headers', 'x-access-token');
   next();
 });
