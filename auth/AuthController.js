edbad6083c050d47082d01c8ac8f890f2ffed5e2
Nguyễn Đăng Tiến <ilikehitech.vn@gmail.com>
2018-10-29 10:14:09 +0000

    Continue fix cors

--- auth/AuthController.js
+++ auth/AuthController.js
@@ -18,6 +18,8 @@ router.use(bodyParser.json());
 
 router.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
+  res.header('Access-Control-Allow-Credentials', true);
+  res.header('Access-Control-Allow-Methods', 'POST, GET, PUT, DELETE, OPTIONS');
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   res.header('Access-Control-Allow-Headers', 'x-access-token');
   next();
